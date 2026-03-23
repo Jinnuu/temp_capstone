cdf48eca45a6e177ba03bb244da17cb1f1a7d0ef
Jinnuu <wlsdngur12@gmail.com>
2026-03-24 02:06:49 +0900

    feat: add current stock calculation and ingredient create flow

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from . import views  # 💡 바로 이 줄이 파이썬에게 views를 알려주는 핵심입니다!
 
-app_name = 'inventory'
+from . import views
+
+app_name = "inventory"
 
 urlpatterns = [
-    # 식재료 목록 화면
-    path('ingredients/', views.ingredient_list, name='ingredient_list'),
-    
-    # 재고 입출고 등록 화면
-    path('log/create/', views.inventory_log_create, name='inventory_log_create'), 
+    path("ingredients/", views.ingredient_list, name="ingredient_list"),
+    path("ingredients/create/", views.ingredient_create, name="ingredient_create"),
+    path("log/create/", views.inventory_log_create, name="inventory_log_create"),
 ]
